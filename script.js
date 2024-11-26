226a262ea4c303db9704b4dfb35d4296876176c7
Urmi Tiwari <urmitiwari1118@gmail.com>
2024-11-26 14:33:24 +0530

    script.js

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ const button  = document.querySelector('button');
 button.addEventListener('click',() =>{
     button.textContent = "You Signed Up!";
 });
- 
+ var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+    mode: "javascript",
+    theme: "dracula",
+    lineNumbers: true,
+    matchBrackets: true
+});
 
+// Function to execute JavaScript code
+function runCode() {
+    var userCode = editor.getValue();
+    try {
+        eval(userCode);
+    } catch (e) {
+        alert('Error: ' + e.message);
+    }
+}
